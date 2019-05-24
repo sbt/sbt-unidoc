db1e32438758a77e94a3df10d03b7b6e599d45f6
Martynas Mickevičius <self@2m.lt>
2019-05-24 14:38:29 +0300

    Update genjavadoc to the latest

--- src/main/scala/sbtunidoc/GenJavadocPlugin.scala
+++ src/main/scala/sbtunidoc/GenJavadocPlugin.scala
@@ -10,7 +10,7 @@ object GenJavadocPlugin extends AutoPlugin {
   }
   import autoImport._
 
-  override def globalSettings = unidocGenjavadocVersion := "0.11"
+  override def globalSettings = unidocGenjavadocVersion := "0.13"
 
   override def requires = JvmPlugin
 
